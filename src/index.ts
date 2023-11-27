306e46f0450abc0b59b8dfb4691f8c9fb38f5c87
nik-shinde-7 <nshinde@matchbot.gg>
2023-11-27 16:12:09 +0530

    chore

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import Badge from "./components/badge";
+
+export { Badge };
---
+++ tailwind.config.ts
@@ -0,0 +1,142 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        MBOceanBlue: {
+          DEFAULT: "#9dd5de",
+        },
+        MBSuccess: {
+          DEFAULT: "#29C871",
+        },
+        MBWarning: {
+          DEFAULT: "#FF8282",
+        },
+        V2MBPrimaryBlueMediumDark: {
+          DEFAULT: "#012544",
+        },
+        V2MBPrimaryBlueLight: {
+          DEFAULT: "#0D639F",
+        },
+        V2MBPrimaryIndigo: {
+          25: "#F5F8FF",
+          50: "#EEF4FF",
+          100: "#E0EAFF",
+          200: "#C7D7FE",
+          300: "#A4BCFD",
+          400: "#8098F9",
+          500: "#6172F3",
+          600: "#444CE7",
+          700: "#3538CD",
+          800: "#2D31A6",
+          900: "#2D3282",
+        },
+        V2MBPrimaryBlue: {
+          25: "#F5FAFF",
+          50: "#EFF8FF",
+          100: "#D1E9FF",
+          200: "#B2DDFF",
+          300: "#84CAFF",
+          400: "#53B1FD",
+          500: "#2E90FA",
+          600: "#1570EF",
+          700: "#175CD3",
+          800: "#1849A9",
+          900: "#194185",
+        },
+        V2MBPrimaryGray: {
+          25: "#FCFCFD",
+          50: "#F9FAFB",
+          100: "#F2F4F7",
+          200: "#EAECF0",
+          300: "#D0D5DD",
+          400: "#98A2B3",
+          500: "#667085",
+          600: "#475467",
+          700: "#344054",
+          800: "#1D2939",
+          900: "#101828",
+        },
+        V2MBError: {
+          25: "#FFFBFA",
+          50: "#FEF3F2",
+          100: "#FEE4E2",
+          200: "#FECDCA",
+          300: "#FDA29B",
+          400: "#F97066",
+          500: "#F04438",
+          600: "#D92D20",
+          700: "#B42318",
+          800: "#912018",
+          900: "#7A271A",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+};
